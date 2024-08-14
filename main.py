28e6be339be68f74b86da20b3f12e56026db142b
RdotSilva <RyandotSilva@gmail.com>
2024-08-14 19:42:18 +0100

    Update debug to show page content

--- main.py
+++ main.py
@@ -6,11 +6,13 @@ load_dotenv()
 
 text_splitter = CharacterTextSplitter(
     separator="\n",
-    chunk_size=200,
+    chunk_size=200,  # Chunk size is checked first and when we hit chunk size then we look for the next separator
     chunk_overlap=0,
 )
 
 loader = TextLoader("facts.txt")
 docs = loader.load_and_split(text_splitter=text_splitter)
 
-print(docs)
+for doc in docs:
+    print(doc.page_content)
+    print("\n")
