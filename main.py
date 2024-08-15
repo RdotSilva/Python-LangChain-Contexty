3736a9c846bd8d7d7582f70deb28dffe210bcb7a
RdotSilva <RyandotSilva@gmail.com>
2024-08-15 20:38:33 +0100

    Add TODO for embeddings

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+embeddings = OpenAIEmbeddings()
+
 text_splitter = CharacterTextSplitter(
     separator="\n",
     chunk_size=200,  # Chunk size is checked first and when we hit chunk size then we look for the next separator
@@ -15,5 +17,6 @@ loader = TextLoader("facts.txt")
 docs = loader.load_and_split(text_splitter=text_splitter)
 
 for doc in docs:
+    # TODO: use embed_query to generate embeddings for each doc and store inside of a vector store
     print(doc.page_content)
     print("\n")
