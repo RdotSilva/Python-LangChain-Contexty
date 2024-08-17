fa1c3ae213802d7f0afc9ec47adc8ccb2396ecd2
RdotSilva <RyandotSilva@gmail.com>
2024-08-17 09:52:54 +0100

    Add basic db setup

--- prompt.py
+++ prompt.py
@@ -1,3 +1,12 @@
 from langchain.embeddings import OpenAIEmbeddings
 from langchain.vectorstores.chroma import Chroma
 from dotenv import load_dotenv
+
+load_dotenv()
+
+embeddings = OpenAIEmbeddings()
+
+db = Chroma(
+    persist_directory="emb",
+    embedding_function=embeddings,
+)
