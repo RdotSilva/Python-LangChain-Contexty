1803586e213a01b67e2728b866a8e1be902992aa
RdotSilva <RyandotSilva@gmail.com>
2024-08-19 21:27:54 +0100

    Add aget_relevant_documents function

--- redundant_filter_retriever.py
+++ redundant_filter_retriever.py
@@ -6,3 +6,6 @@ from langchain.schema import BaseRetriever
 class RedundantFilterRetriever(BaseRetriever):
     def get_relevant_documents(self, query):
         return []
+
+    async def aget_relevant_documents(self):
+        return []
