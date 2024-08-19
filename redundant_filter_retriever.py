f744564e8ccd8c372e8c441b17ac07e1a1526820
RdotSilva <RyandotSilva@gmail.com>
2024-08-19 21:26:40 +0100

    Add RedundantFilterRetriever class

--- redundant_filter_retriever.py
+++ redundant_filter_retriever.py
@@ -1,3 +1,6 @@
 from langchain.embeddings.base import Embeddings
 from langchain.vectorstores import Chroma
 from langchain.schema import BaseRetriever
+
+
+class RedundantFilterRetriever(BaseRetriever):
